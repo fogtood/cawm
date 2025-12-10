33f0112276a8216b339df06e98d90c8e49ba9b0e
fogtood <akandefortunatus2021@gmail.com>
2025-12-10 16:43:15 +0100

    Update API version in sanity client configuration

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -2,7 +2,7 @@ import { createClient } from 'next-sanity'
 
 const projectId = 'fyfkc0lb'
 const dataset = 'production'
-const apiVersion = 'vX'
+const apiVersion = '2025-10-21'
 
 export const client = createClient({
   projectId,
