02ecbc014cc7837f94ccb85f4fa4f244e20faa75
fogtood <akandefortunatus2021@gmail.com>
2025-12-10 16:00:56 +0100

    Update Sanity API version and remove unused options

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -2,15 +2,11 @@ import { createClient } from 'next-sanity'
 
 const projectId = 'fyfkc0lb'
 const dataset = 'production'
-const apiVersion = '2025-07-09'
+const apiVersion = 'vX'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: true,
-  token: process.env.SANITY_VIEWER_TOKEN,
-  stega: {
-    studioUrl: process.env.NEXT_PUBLIC_SANITY_STUDIO_URL,
-  },
 })
